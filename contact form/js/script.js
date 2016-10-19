084ebe2b5a899bbbb332677231fd5a5f65ecf6cb
Kevin <KevinKofii@KevinKofiisMacbookPro.local>
2016-10-19 11:29:57 +0200

    initial commit

---
+++ contact form/js/script.js
@@ -0,0 +1,8 @@
+
+document
+    .querySelectorAll("button")   [0]
+    .addEventListener("click", function() {
+        this.innerHTML = "this button has been clicked";
+        alert("irritating popup");
+    })
+ // function() {}
